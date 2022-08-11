1537e38157299a705bf6c27bcb5256d05c0b1879
kmanadkat <kmanadkat@gmail.com>
2022-08-11 10:20:31 +0530

    Integrated Redux Store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.scss'
+import { Provider } from 'react-redux'
+import { store } from './Store'
 import App from './App'
+
+import './Styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
