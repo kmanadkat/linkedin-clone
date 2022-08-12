31bff23fb5c6768a43093a2eb40a5ebb3c42bee6
kmanadkat <kmanadkat@gmail.com>
2022-08-12 23:26:44 +0530

    Completed Loading Component

--- src/Routes/Loading/index.tsx
+++ src/Routes/Loading/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { LINKEDIN_LOGO_FULL } from '../../Constants'
+
+import './index.scss'
 
 const Loading = () => {
-  return <div>Loading</div>
+  return (
+    <div className="loading">
+      <img src={LINKEDIN_LOGO_FULL} alt="Linkedin Logo" />
+
+      <div className="loading__loader">
+        <div className="loading__bluebar"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Loading
