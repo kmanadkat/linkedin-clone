60ae3a3e1e64d232adbc0e2f44af8c5de42098cc
kmanadkat <kmanadkat@gmail.com>
2022-08-13 14:21:35 +0530

    Widgets Added

--- src/Routes/Home/Widgets.tsx
+++ src/Routes/Home/Widgets.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { WIDGETS } from '../../Constants'
+import './Widgets.scss'
 
 const Widgets = () => {
-  return <div>Widgets</div>
+  return (
+    <div className="widgets">
+      <h3>LinkedIn News</h3>
+
+      {WIDGETS.map(({ url, title, subtitle, key }) => (
+        <div
+          key={key}
+          className="widgets__item"
+          onClick={() => window.open(url, '_blank')}>
+          <div className="dot"></div>
+          <div className="widgets__item--info">
+            <h3>{title}</h3>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Widgets
